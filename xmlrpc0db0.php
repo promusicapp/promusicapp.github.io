90081d8be6ac590651ed2a8324c7cf306f27fec5
promusicapp <promusicapp@163.com>
2018-08-25 18:30:25 +0800

    update

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -4,11 +4,11 @@
     <engineLink>https://wordpress.org/</engineLink>
     <homePageLink>https://www.promusicapps.tk/</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.promusicapps.tk//xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk//xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk//xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk//xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk//wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://www.promusicapps.tk/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://www.promusicapps.tk/wp-json/" />
 	    </apis>
   </service>
 </rsd>
